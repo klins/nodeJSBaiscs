ca89f90e89013fd814096f6e8d16f16d5f5aa288
klins <klins2kool@gmail.com>
2017-05-30 11:52:05 +0530

    first checkin

---
+++ module1.js
@@ -0,0 +1,8 @@
+var a =1;
+
+/*module.exports.a=a;
+module.exports.b=a;*/
+
+module.exports = function(){
+	console.log("module 1 function");
+};
---
+++ module2.js
@@ -0,0 +1,7 @@
+var m2= require('./module1');
+
+var _  = require('underscore');
+//console.log(m2);
+m2();
+
+console.log(_);
